a19e5e4508ca2a477d611ae4237d522c352037ad
JayTruong <Jaytruong@5427gmail.com>
2024-10-28 15:42:30 +0700

    bai test devops

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -2,8 +2,8 @@ module.exports = {
     networks: {
       development: {
         host: "127.0.0.1",
-        port: 7545,  // "Use Ganache or a simulated network."
-        network_id: "*" // Connect any network id
+        port: 7545,  // "Use Ganache or a simulated network." // Sử dụng Ganache hoặc mạng giả lập 
+        network_id: "*" // Connect any network id // cho phép kết nối bất kì network id nào
       },
     },
     compilers: {
